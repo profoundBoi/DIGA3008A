c32376ede13bd376dcb4501354be6fc80954de50
profoundBoi <connor170103@gmail.com>
2025-05-21 22:30:15 +0200

    add image make big

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,12 +1,26 @@
-num = 50;
+document.addEventListener("DOMContentLoaded", () => 
+{
+  const images = document.querySelectorAll("figure img");
 
-sen = "awe awe";
+  images.forEach(img => 
+    {
+        img.addEventListener("click", () => 
+        {
+         const overlay = document.createElement("div");
+         overlay.classList.add("img-overlay");
 
-constant = false;
+         const enlargedImg = document.createElement("img");
+         enlargedImg.src = img.src;
+         enlargedImg.alt = img.alt;
+         enlargedImg.classList.add("enlarged-img");
 
-constant = true;
+         overlay.appendChild(enlargedImg);
+         document.body.appendChild(overlay);
 
-console.log(constant );
-console.log("Hello, My friend!");
-
-//window.alert("Hello, My friend!");
+            overlay.addEventListener("click", () => 
+            {
+             document.body.removeChild(overlay);
+            });
+       });
+   });
+});
